9d9b786cb771008548fac7638a25dedcb3dca7bd
Sergio Gonzalez <andgonzalezmar@unal.edu.co>
2022-01-11 13:58:51 -0500

    Pagina de inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { iniciarSesionComponent } from './components/iniciarSesion/iniciarSesion.component';
 import { LoginComponent } from './components/login/login.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    iniciarSesionComponent
+    iniciarSesionComponent,
+    InicioComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
